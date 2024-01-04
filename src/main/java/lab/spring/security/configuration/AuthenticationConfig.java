7b3601764092f573987e3589ee08fbb4543a710b
uknow-home <yoon6763@naver.com>
2024-01-04 22:07:44 +0900

    스프링 시큐리티 Config 파일 추가

---
+++ src/main/java/lab/spring/security/configuration/AuthenticationConfig.java
@@ -0,0 +1,11 @@
+package lab.spring.security.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class AuthenticationConfig {
+    
+}
