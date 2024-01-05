921a3c78a3ec98f4bf3fea5c98f8479c4af13672
uknow-home <yoon6763@naver.com>
2024-01-05 12:38:43 +0900

    스프링 시큐리티 초기 세팅

--- src/main/java/lab/spring/security/configuration/AuthenticationConfig.java
+++ src/main/java/lab/spring/security/configuration/AuthenticationConfig.java
@@ -1,11 +1,34 @@
 package lab.spring.security.configuration;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class AuthenticationConfig {
-    
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic(AbstractHttpConfigurer::disable) // UI가 아닌 토큰으로 인증을 할 것
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/api/v1/users/login").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/api/v1/reviews").authenticated()
+                        .anyRequest().authenticated())
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionFixation().changeSessionId())
+//                .addFilterBefore(new JwtTokenFilter(userService, secretKey)
+//                        , UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+    }
 }
